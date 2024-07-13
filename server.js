21d164d23ea41795be074499c51ebb4d7e45b16d
Bhavuk01 <bhavuk1668.be21@chitkara.edu.in>
2024-07-13 23:37:30 +0530

    .

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: 'https://minesh.netlify.app',
+  origin: ['http://127.0.0.1:5500', 'https://minesh.netlify.app'],
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type']
 }));
@@ -15,15 +15,15 @@ app.use(bodyParser.json());
 const validKeys = ['your-key-1', 'your-key-2', 'your-key-3'];
 
 app.post('/api/login', (req, res) => {
-    const { key } = req.body;
-    if (validKeys.includes(key)) {
-        res.json({ success: true });
-    } else {
-        res.json({ success: false });
-    }
+  const { key } = req.body;
+  if (validKeys.includes(key)) {
+    res.json({ success: true });
+  } else {
+    res.json({ success: false });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
